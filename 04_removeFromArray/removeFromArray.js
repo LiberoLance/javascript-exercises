87157e705deb8a00b3b02bc2ef34a3710a70bf3d
Libero Lance <mfc2294@gmail.com>
2022-04-15 17:12:31 -0400

    Fix snytax error and include a return

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,12 @@
 const removeFromArray = function(array, ...rmvelem) {
-  if(array.includes(rmvelem) {
+  if(array.includes(rmvelem)) {
     for(let i = 0; i < array.length - 1; i++) {
       if(array[i] === rmvelem) {
         array.splice(i,1);
       }
     }
   }
+  return array;
 };
 
 // Do not edit below this line
