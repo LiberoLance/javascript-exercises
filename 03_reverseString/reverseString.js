6a0a0d4d7cbf31cd0dc92892bb8b0012c56b07d3
Libero Lance <mfc2294@gmail.com>
2022-04-14 21:16:35 -0400

    Fix logic error

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -2,9 +2,6 @@ const reverseString = function(str) {
   let rtrstr = '';
   for(let i = str.length - 1; i >= 0; i--) {
     rtrstr += str.charAt(i);
-    console.log(str);
-    console.log(str.charAt(i));
-    console.log(rtrstr);
   }
   return rtrstr;
 };
