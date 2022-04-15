2558321a0bee765b61daf67a8d5e1789e4b2ad63
Libero Lance <mfc2294@gmail.com>
2022-04-14 21:14:33 -0400

    Edit

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,8 +1,10 @@
 const reverseString = function(str) {
   let rtrstr = '';
   for(let i = str.length - 1; i >= 0; i--) {
-    rtrstr += str.charAt[i];
+    rtrstr += str.charAt(i);
     console.log(str);
+    console.log(str.charAt(i));
+    console.log(rtrstr);
   }
   return rtrstr;
 };
