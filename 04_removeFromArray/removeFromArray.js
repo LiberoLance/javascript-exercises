eca15afe1a867cfbbdc524bd521710468dc63628
Libero Lance <mfc2294@gmail.com>
2022-04-15 17:10:23 -0400

    Write remove function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...rmvelem) {
+  if(array.includes(rmvelem) {
+    for(let i = 0; i < array.length - 1; i++) {
+      if(array[i] === rmvelem) {
+        array.splice(i,1);
+      }
+    }
+  }
 };
 
 // Do not edit below this line
