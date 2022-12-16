530f16f418f95167c6b80a5be3755cb2a1f73c8e
Libero Lance <mfc2294@gmail.com>
2022-12-16 17:42:41 -0500

    Add code and test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (text) {
+	let palindrome = false;
+	let compText = '';
+	text = text.trim();
+	text = text.toLowerCase();
+	let n = text.length;
 
+	for(i = 0; i < n; i++){
+		if(text[i] >= 'a' && text[i] <= 'z'){
+			compText += text[i]
+		}
+	}
+	const reverse = compText.split('').reverse().join('');
+	console.log(compText);
+	console.log(reverse);
+	if(compText == reverse){
+		palindrome = true;
+	}
+	return palindrome;
 };
 
 // Do not edit below this line
