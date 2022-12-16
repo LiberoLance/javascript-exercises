11e8e5b734ea8ba89995482a703ddb40b3f2c3ee
Libero Lance <mfc2294@gmail.com>
2022-12-16 11:42:58 -0500

    Add code and test

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,52 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(list) {
+	let sum = 0;
+	for(const num of list) {
+		sum += num;
+ 	}
+	return sum;
 };
 
-const multiply = function() {
+const multiply = function(list) {
+	let product = 1;
 
+	for(const num of list) {
+		product *= num;
+	}
+	return product;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	const base = x;
+
+	if(y == 0) {
+		x = 1;
+	}
+	while(y > 1){
+		x *= base;
+		y--;
+	}
+	return x;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let product = x;
+	if(x == 0) {
+		product = 1;
+	}
+	x--;
+	while(x > 1){
+		product *=  x;
+		x--;
+	}
+	return product;
 };
 
 // Do not edit below this line
