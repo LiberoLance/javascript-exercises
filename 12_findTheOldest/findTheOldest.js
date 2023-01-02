50d8138b7c66a65ed2cb57acae9290a3be2ad682
Libero Lance <mfc2294@gmail.com>
2023-01-01 21:29:33 -0500

    Add map function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+	let oldest = people.reduce(function (lastPerson, currentPerson) {
+		let oldPerson = currentPerson;
+		
+		if(lastPerson.yearOfDeath != null && currentPerson.yearOfDeath != null){ 
+			oldPerson = lastPerson.yearOfDeath - lastPerson.yearOfBirth > currentPerson.yearOfDeath - currentPerson.yearOfBirth ? lastPerson : currentPerson;
+		}
+		else{
+			oldPerson = lastPerson.yearOfBirth < currentPerson.yearOfBirth ? lastPerson : currentPerson;
+		}
 
+		
+		return oldPerson;
+	});
+
+	return oldest;
 };
 
 // Do not edit below this line
